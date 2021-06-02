246483aff50fd750296507c585243e4d5cc95f74
渡辺孝之 <na8esin.mpst@gmail.com>
2021-06-02 13:13:33 +0900

    次の単語を取り出すロジック

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -6,6 +6,10 @@ import androidx.lifecycle.ViewModel
 class GameViewModel : ViewModel() {
     private var score = 0
     private var currentWordCount = 0
+
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+
     private var _currentScrambledWord = "test"
     val currentScrambledWord: String
         get() = _currentScrambledWord
@@ -17,5 +21,15 @@ class GameViewModel : ViewModel() {
     override fun onCleared() {
         super.onCleared()
         Log.d("GameFragment", "GameViewModel destroyed!")
+    }
+
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+
+        // シャッフルしても元の単語と同じなら繰り返す
+        while (tempWord.toString().equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
     }
 }
