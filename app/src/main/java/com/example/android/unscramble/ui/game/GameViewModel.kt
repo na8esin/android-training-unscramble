12a8b303bc6a8dfdcde75e6b5f89e93b57bc54ec
渡辺孝之 <na8esin.mpst@gmail.com>
2021-06-02 14:53:19 +0900

    最終スコアを表示するダイアログを追加

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -4,7 +4,10 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
-    private var score = 0
+    private var _score = 0
+    val score: Int
+        get() = _score
+
     private var currentWordCount = 0
 
     private var wordsList: MutableList<String> = mutableListOf()
