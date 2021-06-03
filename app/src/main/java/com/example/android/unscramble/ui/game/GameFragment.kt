002d4fd94c7171df293e35733ab5f973674f568a
渡辺孝之 <na8esin.mpst@gmail.com>
2021-06-03 09:37:11 +0900

    ゲームを初期化する処理を追加

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -102,6 +102,7 @@ class GameFragment : Fragment() {
      * restart the game.
      */
     private fun restartGame() {
+        viewModel.reinitializeData()
         setErrorTextField(false)
         updateNextWordOnScreen()
     }
